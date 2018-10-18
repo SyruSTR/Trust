1c489bfcabb5516cd081a84da1343596d3de0a3c
SyruSTR <syrus_tr@mail.ru>
2018-10-18 23:57:50 +0800

    Move Controller

--- Assets/Scripts/NewScript/MovementController.cs
+++ Assets/Scripts/NewScript/MovementController.cs
@@ -35,18 +35,50 @@ public class MovementController : MonoBehaviour
 
     public void Move(Vector2 _input, bool _isAcceleration)
     {
-        if(_input != Vector2.zero)
+        if (_input != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(_input.x, _input.y) * Mathf.Rad2Deg + camera.eulerAngles.y;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref curentVelocitySpeed, smoothTime);
+        }
 
-            //float targetSpped = 
+            float targetSpeed = (_isAcceleration ? runSpeed : walkSpeed) * _input.magnitude;
+            currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref curentVelocitySpeed, smoothTime);
 
+            Vector3 velocity = transform.forward * currentSpeed + Vector3.up * gravity + Vector3.up * jumpVelocity;
+
+            characterController.Move(velocity * Time.deltaTime);
+
+            if (jumpVelocity > 0)
+                jumpVelocity += gravity * Time.deltaTime;
+            if (jumpVelocity < 0)
+                jumpVelocity = 0;
+
+            animator.SetBool("isMove", targetSpeed != 0);
+            animator.SetFloat("movementSpeed", targetSpeed * 0.5f);
+            animator.SetBool("isGround", characterController.isGrounded);
+            if (characterController.isGrounded)
+                animator.SetFloat("groudDistance", GetGroundDistance());
+        
+    }
+
+    public void Jump()
+    {
+        if (characterController.isGrounded)
+        {
+            jumpVelocity = -gravity * jumpForce;
+            animator.SetTrigger("Jump");
         }
     }
 
-    void Update()
+    private float GetGroundDistance()
     {
-        
+        float downPoint = transform.position.y + characterController.center.y - characterController.height * 0.5f;
+        Vector3 startPoint = transform.position;
+
+        if (Physics.Raycast(startPoint, Vector3.down, out RaycastHit hit))
+        {
+            return (float)System.Math.Round(hit.distance, 2);
+        }
+        return float.MaxValue;
     }
 }
---
+++ Assets/Scripts/NewScript/PlayerInput.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementController))]
+public class PlayerInput : MonoBehaviour
+{
+    public KeyCode moveForward = KeyCode.W;
+    public KeyCode moveBack = KeyCode.S;
+    public KeyCode moveLeft = KeyCode.A;
+    public KeyCode moveRight = KeyCode.D;
+    [Space]
+    public KeyCode jump = KeyCode.Space;
+    [Space]
+    public KeyCode acceleration = KeyCode.LeftShift;
+
+    private MovementController movementController;
+
+    void Start()
+    {
+        movementController = GetComponent<MovementController>();
+    }
+
+    void Update()
+    {
+        Vector2 input = Vector2.zero;
+
+        if (Input.GetKey(moveForward))
+            input.y = 0.1f;
+        else if (Input.GetKey(moveBack))
+            input.y = -0.1f;
+
+        if (Input.GetKey(moveLeft))
+            input.x = -0.1f;
+        else if (Input.GetKey(moveRight))
+            input.x = 0.1f;
+
+        if (Input.GetKey(jump))
+            movementController.Jump();
+
+        movementController.Move(input, Input.GetKey(acceleration));
+    }
+}
